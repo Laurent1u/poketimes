9ac2d472ee2ff040d567ad2c7f5693f625857676
Larry <pascu.laurentiu17@gmail.com>
2019-11-28 17:58:19 +0200

    Updates

---
+++ precache-manifest.5b0777a0befe6bed8b5349a61b71c487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9001db7b81dc6b7dba70e5384d0f4e6c",
+    "url": "/poketimes/index.html"
+  },
+  {
+    "revision": "4cda197607e4b3ffeeeb",
+    "url": "/poketimes/static/css/main.f9c04ee0.chunk.css"
+  },
+  {
+    "revision": "8ddd6aa8f3a9173bf7ba",
+    "url": "/poketimes/static/js/2.0ad0310c.chunk.js"
+  },
+  {
+    "revision": "4cda197607e4b3ffeeeb",
+    "url": "/poketimes/static/js/main.7dff036b.chunk.js"
+  },
+  {
+    "revision": "566243c02e3829feb38f",
+    "url": "/poketimes/static/js/runtime-main.f85248bd.js"
+  }
+]);
